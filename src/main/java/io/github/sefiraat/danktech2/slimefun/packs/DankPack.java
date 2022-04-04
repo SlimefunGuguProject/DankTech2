b76239cbd03443b720996cdeaa3afc6bba3190e3
ybw0014 <i@guizhanss.com>
2022-04-04 14:24:44 -0400

    尝试修复刷物品

--- src/main/java/io/github/sefiraat/danktech2/slimefun/packs/DankPack.java
+++ src/main/java/io/github/sefiraat/danktech2/slimefun/packs/DankPack.java
@@ -5,11 +5,16 @@ import io.github.thebusybiscuit.slimefun4.api.items.ItemSetting;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.items.settings.IntRangeSetting;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.DistinctiveItem;
 import io.github.thebusybiscuit.slimefun4.implementation.items.blocks.UnplaceableBlock;
 import lombok.Getter;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 
-public class DankPack extends UnplaceableBlock {
+import javax.annotation.Nonnull;
+
+public class DankPack extends UnplaceableBlock implements DistinctiveItem {
 
     @Getter
     private final int tier;
@@ -24,5 +29,10 @@ public class DankPack extends UnplaceableBlock {
         this.slots = tier;
         this.capacityPerSlot = new IntRangeSetting(this, "capacity_per_slot", 1, defaultCapacity, Integer.MAX_VALUE);
         addItemSetting(capacityPerSlot);
+    }
+
+    @Override
+    public boolean canStack(@Nonnull ItemMeta sfItemMeta, @Nonnull ItemMeta itemMeta) {
+        return sfItemMeta.getPersistentDataContainer().equals(itemMeta.getPersistentDataContainer());
     }
 }
